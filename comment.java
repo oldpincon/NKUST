36e14f70c5cb1c416d21f791bc6a78df1c45315a
oldpincon <93572382+oldpincon@users.noreply.github.com>
2021-11-16 14:18:36 +0800

    Add files via upload

---
+++ comment.java
@@ -0,0 +1,7 @@
+public class comment {
+    public static void main(String[] args) {
+        //This is comment
+        //HI HI HI
+        //這是註解
+    }
+}
---
+++ hentai.java
@@ -0,0 +1,10 @@
+public class hentai {
+    public static void main(String[] args) {
+        String av = "黃敬綸的長度";
+        int number = 9;
+        String unit = "cm";
+        System.out.println(av + number + unit);
+
+
+    }
+}
