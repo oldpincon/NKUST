b2144095fd1e97164358a684295d963d1c49ad68
c110181132 <93572393+c110181132@users.noreply.github.com>
2021-12-21 01:36:41 +0800

    Add files via upload

---
+++ berserker.java
@@ -0,0 +1,11 @@
+public class berserker extends Role{
+    int HP;
+    int MP;
+    int ATTACK;
+
+    berserker(int roleHP,int roleMP ,int roleATTACK) {
+
+        super(roleHP, roleMP, roleATTACK);
+    }
+
+}
